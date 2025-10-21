bb5e81d78708ba1de6a90e1bd5d6f86b0271ca5f
So Man Tik <1463647948qq@gmail.com>
2025-10-21 17:20:58 +0800

    add toggleFavorite

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Namer App',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 121, 228, 221)),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 121, 228, 221),
+          ),
         ),
         home: MyHomePage(),
       ),
@@ -31,13 +33,23 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+    var favorites = <WordPair>[];
+
+   void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
- var pair = appState.current;
+    var pair = appState.current;
     return Scaffold(
       body: Center(
         child: Column(
@@ -60,27 +72,27 @@ class MyHomePage extends StatelessWidget {
 }
 
 class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
+  const BigCard({super.key, required this.pair});
 
   final WordPair pair;
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);       // ← Add this.
+    final theme = Theme.of(context); // ← Add this.
     final style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
     );
 
     return Card(
-      color: theme.colorScheme.primary,    // ← And this.
+      color: theme.colorScheme.primary, // ← And this.
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style,
-        
-        semanticsLabel: "${pair.first} ${pair.second}"),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
